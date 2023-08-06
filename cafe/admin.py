65cab78133f9978ebe1561b76c3f7b911107c638
your_github_username <your_github_email>
2023-08-07 00:22:52 +0330

    feat: change panel admin

--- cafe/admin.py
+++ cafe/admin.py
@@ -4,12 +4,13 @@ from .models import Category,Product ,Customer
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display =('name' ,)
 
 @admin.register(Product)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display =('name' , 'price' , 'is_available' , 'category')
+    list_filter = ('is_available' ,'category')
 
 @admin.register(Customer)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display =('name' , 'phone_number')
