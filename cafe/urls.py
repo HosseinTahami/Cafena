a8c66b5451f8e8c282e5e00d19e0073b45ead43e
AnahitaDzl <anahita.dozali@gmail.com>
2023-08-23 07:17:25 +0330

    feat: about url added

--- cafe/urls.py
+++ cafe/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 # inner modules imports
-from .views import SearchView, HomeView, ProductDetailView
+from .views import SearchView, HomeView, ProductDetailView , AboutView
 
 app_name = "cafe"
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("search_results/", SearchView.as_view(), name="search_results"),
     path("product_detail/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
+    path("about/" , AboutView.as_view() , name='about'),
 ]
