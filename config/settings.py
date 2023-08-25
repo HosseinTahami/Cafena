e4c6ce244b75558bba31299d10861ecf81bd6ef7
mabrishami00 <mabrishami78@yahoo.com>
2023-08-25 22:34:41 +0330

    chore: add staticfiles to gitignore and static root

--- config/settings.py
+++ config/settings.py
@@ -165,8 +165,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "cafe", "static"),
     os.path.join(BASE_DIR, "accounts", "static"),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
