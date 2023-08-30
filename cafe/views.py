65b75a41687033ae7aab9edb6d60404278dcbdd8
mabrishami00 <mabrishami78@yahoo.com>
2023-08-31 02:30:49 +0330

    refactor: change the redirect

--- cafe/views.py
+++ cafe/views.py
@@ -1,10 +1,10 @@
 # django imports
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.views import View
 
 # inner modules imports
-from .models import Product, Category , Contact
+from .models import Product, Category, Contact
 from orders.forms import CartAddForm
 from django.db.models import Q
 from dynamic.models import PageData
@@ -52,19 +52,21 @@ class ProductDetailView(DetailView):
         context["form"] = CartAddForm()
         return context
 
+
 class AboutView(View):
     def get(self, request):
-        return render(request , 'cafe/about.html')
+        return render(request, "cafe/about.html")
+
 
 class ContactView(View):
     def get(self, request):
-        return render(request , 'cafe/contact.html')
+        return render(request, "cafe/contact.html")
 
-    def post(self , request):
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
+    def post(self, request):
+        name = request.POST["name"]
+        email = request.POST["email"]
+        subject = request.POST["subject"]
+        message = request.POST["message"]
         contact = Contact(name=name, email=email, subject=subject, message=message)
         contact.save()
-        return render(request , 'cafe/home.html')
+        return redirect("cafe:home")
